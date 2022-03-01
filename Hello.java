7cf1a16e30d2863daf329fd867249bb02d019d03
dilshad <dshaikh439@gmail.com>
2022-03-01 15:55:50 -0500

    resolve conflict

--- Hello.java
+++ Hello.java
@@ -7,14 +7,14 @@ public class Hello
 		System.out.println(" value of a"+a);
 		
 		System.out.println(" make a change in file");
-<<<<<<< HEAD
+
 	
 		System.out.println( "you also add at same time");
-=======
+
 		
 		System.out.println("another one added data");
 
->>>>>>> a161defb0a8c9b9e3f6397b88b3f63e5093de567
+
 	}
 	
 }
